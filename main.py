8716c9773da0ccbbfad0ec2b6d21bb751c32aad7
Khanh Tran <vktran@stanford.edu>
2024-03-16 01:00:27 -0700

    Add function comments

--- main.py
+++ main.py
@@ -5,8 +5,24 @@ import matplotlib.pyplot as plt
 import noise
 
 
-# Generate simple landscape
 def generate_landscape(width, height, scale):
+    """
+    Function to generate landscape for an image
+    of a given width and height.
+
+    The scale determines how much variance the overall
+    image should have.
+
+    It does this via the following steps:
+    1. Determine the sky base color and blend function via calls to custom random functions
+    2. Loop through each pixel in the image and generate a horizon line via samples
+    from a 1 dimensional perlin noise spatial distribution
+    3. Fill in the image with sky or grass depending on if its below or above the horizon
+    4. Add clouds via a custom random function
+    5. Add rocks via a custom random function
+    6. Add trees via a custom random function
+    """
+
     color_world = np.zeros((height, width, 3), dtype=np.uint8)
 
     # Get base sky color and sky color blend factor function
@@ -31,13 +47,22 @@ def generate_landscape(width, height, scale):
 
 
 def add_clouds(color_world, width, height, scale):
+    """
+    Function to add clouds to an image
+
+    The function does this via the following steps
+    1. Use a geometric distribution to determine the number of cloud
+    2. Use a uniform distribution to determine a random location
+    for each cloud somewhere above the hill horizon
+    3. Calls the function to actually draw the clouds themselves
+    """
     num_of_clouds = int(np.random.geometric(1 / 5))
 
     cloud_locations = list()
     for i in range(num_of_clouds):
         x = int(np.random.uniform(0, width))
         hill_height = int(noise.pnoise1(x * scale, octaves=2) * height / 4 + height / 2)
-        y = int(np.random.uniform(0, hill_height-300))
+        y = int(np.random.uniform(0, hill_height - 300))
         cloud_locations.append([x, y])
 
     for x, y in cloud_locations:
@@ -45,6 +70,16 @@ def add_clouds(color_world, width, height, scale):
 
 
 def draw_clouds(color_world, x, y, height, width, scale):
+    """
+    This function draws a single cloud at the requested location
+
+    The function does this via the following steps
+    1. Determines the aspec ratio of the cloud via a uniform distribution
+    2. Determine the size of the cloud via a normal distribution
+    3. Scale the size based on how close it is to the horizon
+    4. Draws the cloud with some added fuzziness via a custom spatial distribution
+    implemented with based on multiple samples from a uniform distribution
+    """
     aspect_ratio = np.random.uniform(1.5, 2.5)
     imperfection = scale * 500
 
@@ -63,6 +98,16 @@ def draw_clouds(color_world, x, y, height, width, scale):
 
 
 def add_rocks(color_world, width, height, scale):
+    """
+    Function to add rocks to an image
+
+    The function does this via the following steps
+    1. Use a geometric distribution to determine the number of rocks
+    2. Use a uniform distribution to determine a random location
+    for each rock somewhere below the hill horizon
+    3. Calls the function to actually draw the rocks themselves
+    """
+
     num_of_rocks = int(np.random.geometric(1 / 5))
 
     rock_locations = list()
@@ -73,10 +118,21 @@ def add_rocks(color_world, width, height, scale):
         rock_locations.append([x, y])
 
     for x, y in rock_locations:
-        draw_rocks(color_world, x, y, height, width, scale)
+        draw_rock(color_world, x, y, height, width, scale)
 
 
-def draw_rocks(color_world, x, y, height, width, scale):
+def draw_rock(color_world, x, y, height, width, scale):
+    """
+    This function draws a single rock at the requested location
+
+    The function does this via the following steps
+    1. Determines the aspect ratio of the rock via a uniform distribution
+    2. Determine the size of the rock via a normal distribution
+    3. Scale the size based on how close it is to the horizon
+    4. Draws the rock with some added noisy imperfections via a custom spatial distribution
+    implemented with based on multiple samples from a uniform distribution
+    """
+
     aspect_ratio = np.random.uniform(1.2, 2)
     imperfection = scale * 100
 
@@ -88,12 +144,25 @@ def draw_rocks(color_world, x, y, height, width, scale):
     for i in range(-radius_y, radius_y + 1):
         for j in range(-radius_x, radius_x + 1):
             if ((j / (radius_x * (1 + np.random.uniform(-imperfection, imperfection)))) ** 2 +
-                (i / (radius_y * (1 + np.random.uniform(-imperfection, imperfection)))) ** 2) <= 1:
+            (i / (radius_y * (1 + np.random.uniform(-imperfection, imperfection)))) ** 2) <= 1:
                 if 0 < y - i < height and 0 < x + j < width:
                     color_world[y - i, x + j] = rock_color
 
 
 def add_trees(color_world, width, height, scale):
+    """
+    Function to add trees to an image
+
+    The function does this via the following steps
+    1. Use a binomial distribution to determine the number of forests
+    2. Use a uniform distribution to determine a random location
+    for each forest somewhere below the hill horizon
+    3. Pick the season from a uniform distribution
+    4. Loop through every location below the horizon and check if
+    a tree should be placed there according to an RBF Spatial Distribution
+    with centers set the locations of the forest. If it should be placed,
+    draw it
+    """
     # Generate list of centers
     num_of_centers = np.random.binomial(3, 0.5)
     centers = list()
@@ -115,6 +184,16 @@ def add_trees(color_world, width, height, scale):
 
 
 def draw_tree(color_world, x, y, height, width, season, scale):
+    """
+    This function draws a single tree at the requested location
+
+    The function does this via the following steps
+    1. Set the bark and leaf colors in accordance with the season
+    2. Set the tree height and width based on how close it is to the horizon
+    3. Scale the tree height and width randomly via a sample from a normal distribution
+    4. Draw the trunk
+    5. Draw the leaves
+    """
     if season == 0:  # Spring
         bark_color = np.array([140, 70, 20])  # Lighter brown for spring
         leaf_color = np.array([60, 179, 113])  # Light green for spring leaves
@@ -155,24 +234,19 @@ def draw_tree(color_world, x, y, height, width, season, scale):
                     color_world[y - trunk_height - i][x + j] = leaf_color
 
 
-def rbf_distribution(x, y, centers, sigma):
-    probability = 0
-    for cx, cy in centers:
-        distance = np.sqrt((x - cx) ** 2 + (y - cy) ** 2)
-        probability += np.exp(-distance ** 2 / (2 * sigma ** 2))
-    return 1 == bernoulli.rvs(probability / 10)
-
-
 def get_base_sky_color():
     """
-    Maps a value in terms of standard deviations from the mean to a color gradient.
-    Returns:
-    - A tuple representing the RGB values of the corresponding color.
+    A Function to randomly determine the base sky color
+
+    It does this via the following steps:
+
+    1. Sample from a normal distribution
+    2. Choose a color based on the sign of the sample
+    3. Blend the chosen color with a third base color based
+    on the sample
     """
 
-    mean, std_dev = 0, 1
-    sample = np.random.normal(mean, std_dev)
-    z_score = (sample - mean) / std_dev
+    sample = np.random.normal(0, 1)
 
     # Define the colors
     mean_color = np.array([0, 0, 139])  # Dark blue
@@ -180,13 +254,21 @@ def get_base_sky_color():
     right_color = np.array([255, 160, 122])  # Pinkish reddish orange
 
     # Interpolate between mean_color and left_color/right_color
-    if z_score < 0:
-        return blend_colors(mean_color, left_color, abs(z_score))
+    if sample < 0:
+        return blend_colors(mean_color, left_color, abs(sample))
     else:
-        return blend_colors(mean_color, right_color, abs(z_score))
+        return blend_colors(mean_color, right_color, abs(sample))
 
 
 def get_sky_color_blend_factor_fnc(height):
+    """
+    This function returns a lambda function that is used to
+    determine the amount by which the base sky color should be blended
+    with "white" based on a given y value.
+
+    It does this by generating a linear lambda function via a sample
+    from a truncated normal distribution
+    """
     horizon_mean = height / 4
     horizon_std_dev = height / 8
     lower_bound = 0
@@ -199,6 +281,14 @@ def get_sky_color_blend_factor_fnc(height):
 
 
 def get_hill_color(x, y, scale, hill_height):
+    """
+    This function gets the hill color of a given point
+
+    It does this via the following steps:
+    1. Set the two base grass colors
+    2. Sample from a perlin noise spatial distribution
+    3. Blend the two colors by an amount determined from the above sample
+    """
     # Set base grass colors
     grass_color_1 = np.array([65, 152, 10])
     grass_color_2 = np.array([19, 133, 16])
@@ -211,8 +301,18 @@ def get_hill_color(x, y, scale, hill_height):
     return blend_colors(grass_color_1, grass_color_2, blend_factor)
 
 
+# This function blends two colors together by amount x
 def blend_colors(color1, color2, x):
     return np.round(color1 + (color2 - color1) * x).astype(int)
+
+
+# Custom implementation of a Radial basis function distribution
+def rbf_distribution(x, y, centers, sigma):
+    probability = 0
+    for cx, cy in centers:
+        distance = np.sqrt((x - cx) ** 2 + (y - cy) ** 2)
+        probability += np.exp(-distance ** 2 / (2 * sigma ** 2))
+    return 1 == bernoulli.rvs(probability / 10)
 
 
 def main():
